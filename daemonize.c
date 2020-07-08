555a89882749d0aa62a9419c15dd7debbb5ec3fd
adamhastings <itsadamhastings@gmail.com>
2020-07-08 14:25:58 -0700

    first daemon. Working!

---
+++ daemonize.c
@@ -0,0 +1,72 @@
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <syslog.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <stdio.h>
+
+static void skeleton_daemon() {
+    
+    pid_t pid;
+
+    // Fork off the parent process
+    pid = fork();
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    // If we get a good PID, then we can exit the parent process
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    // On success: The child becomes session leader
+    if (setsid() < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    // Catch, ignore, and handle signals
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGHUP, SIG_IGN);
+
+    // Fork off for the second time
+    pid = fork();
+
+    // An error occurred...
+    if (pid < 0) {
+        exit(EXIT_FAILURE);
+    }
+
+    // Success! Let the parent terminate
+    if (pid > 0) {
+        exit(EXIT_SUCCESS);
+    }
+
+    // Change the file mode mask
+    umask(0);
+
+    // Close all file descriptors
+    int x;
+    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--) {
+        close(x);
+    }
+
+    openlog("firstdaemon", LOG_PID, LOG_DAEMON);
+}
+
+int main() {
+    skeleton_daemon();
+    
+    while(1) {
+        syslog(LOG_NOTICE, "First daemon started.");
+        sleep(20);
+        break;
+    }
+
+    syslog(LOG_NOTICE, "First daemon terminated.");
+    closelog();
+
+    return EXIT_SUCCESS;
+}
