f22f6a3778ab411704e23c12932fa636343b71da
adamhastings <itsadamhastings@gmail.com>
2020-07-27 12:03:16 -0700

    expsearch cleaned up. runs from main script now

--- protocol1/expsearch.py
+++ protocol1/expsearch.py
@@ -2,50 +2,52 @@ import os
 import sys
 
 
-file1 = open("results.txt", "a")
-os.system('clear')
-name = input("Please type your name: ")
-os.system('cls' if os.name == 'nt' else 'clear')
-x = 1
-slowdown = str(20)
-accept = False
+def get_WTA():
+    #file1 = open("results.txt", "a")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    #name = input("Please type your name: ")
+    os.system('cls' if os.name == 'nt' else 'clear')
+    x = 1
+    slowdown = str(20)
+    accept = False
 
 
 
-while (not accept):
-    response = input("Would you be willing to accept a permanent " + slowdown + "% slowdown on your own computer in exchange for \n\n\t$" + str(x) + "?\n\n Type yes or no: ")
-    if (response.lower() == "y" or response.lower() == "yes"):
-        accept = True
-    elif (response.lower() == "n" or response.lower() == "no"):
-        x *= 2
-        os.system('cls' if os.name == 'nt' else 'clear')
-    else:
-        print("Please type 'yes' or 'no'")
+    while (not accept):
+        response = input("Would you be willing to accept a permanent " + slowdown + "% slowdown on your own computer in exchange for \n\n\t$" + str(x) + "?\n\n Type y/n: ")
+        if (response.lower() == "y" or response.lower() == "yes"):
+            accept = True
+        elif (response.lower() == "n" or response.lower() == "no"):
+            x *= 2
+            os.system('cls' if os.name == 'nt' else 'clear')
+        else:
+            print("\n\nPlease type 'y' or 'n'\n\n")
 
-if (x < 8):
-    print("Thanks!")
-    WTA = x
-    file1.write(name + "," + str(WTA) + "\n")
-    file1.close()
-    sys.exit()
-
-os.system('cls' if os.name == 'nt' else 'clear')
-
-upper = x
-lower = x/2
-while (upper - lower > 2):
-    midpoint = int((upper + lower)/2)
-    response = input("Would you be willing to accept a permanent " + slowdown + "% slowdown on y    our own computer in exchange for \n\n\t$" + str(midpoint) + "?\n\n Type yes or no: ")
-
-    if (response.lower() == "y" or response.lower() == "yes"):
-        upper = midpoint        
-    else:
-        lower = midpoint
+    if (x < 8):
+        WTA = x
+        #file1.write(name + "," + str(WTA) + "\n")
+        #file1.close()
+        return WTA
 
     os.system('cls' if os.name == 'nt' else 'clear')
 
-print("Thanks!")
-WTA = midpoint
-file1.write(name + "," + str(WTA) + "\n")
-file1.close()
-sys.exit()
+    upper = x
+    lower = x/2
+    while (upper - lower > 2):
+        midpoint = int((upper + lower)/2)
+        response = input("Would you be willing to accept a permanent " + slowdown + "% slowdown on your own computer in exchange for \n\n\t$" + str(midpoint) + "?\n\n Type y/n: ")
+
+        if (response.lower() == "y" or response.lower() == "yes"):
+            upper = midpoint        
+        elif (response.lower() == "n" or response.lower() == "no"):
+            lower = midpoint
+        else:
+            print("\n\nPlease type 'y' or 'n'\n\n")
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+    WTA = midpoint
+    #file1.write(name + "," + str(WTA) + "\n")
+    #file1.close()
+    return WTA
---
+++ protocol1/main.py
@@ -0,0 +1,5 @@
+from expsearch import get_WTA
+
+WTA= get_WTA()
+
+print(WTA)
