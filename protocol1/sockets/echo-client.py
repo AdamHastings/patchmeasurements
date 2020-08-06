28ff3d4a4809b48a663576fc08f7fb1c24dea28e
adamhastings <itsadamhastings@gmail.com>
2020-08-06 16:56:14 -0400

    python scripts

--- protocol1/sockets/echo-client.py
+++ protocol1/sockets/echo-client.py
@@ -1,6 +1,9 @@
+#!/usr/bin/env python3
+
 import socket
 
-HOST = '127.0.0.1'  # The server's hostname or IP address
+#HOST = 'www.cs.columbia.edu/~hastings/'  # The server's hostname or IP address
+HOST = '127.0.0.1'
 PORT = 65432        # The port used by the server
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
