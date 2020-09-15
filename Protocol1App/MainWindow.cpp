f6dc9e8fa8b299ce0e3b51c72348da380d92ce2e
adamahastings <hastings@cs.columbia.edu>
2020-09-15 10:18:44 -0400

    added power stuff. probably should be added to its own class

--- Protocol1App/MainWindow.cpp
+++ Protocol1App/MainWindow.cpp
@@ -5,6 +5,12 @@
 #include <thread>
 #include <chrono>
 #include <stdlib.h>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <iostream>
+#include <QProcess>
+using namespace std;
 
 void MainWindow::showGoodbye() {
     ui.stackedWidget->setCurrentWidget(ui.goodbye);
@@ -20,6 +26,68 @@ void MainWindow::showTask2() {
 
 void MainWindow::showTask3() {
     ui.stackedWidget->setCurrentWidget(ui.task3);
+}
+
+vector<int> parsePowercfgOutput(string s) {
+    // gets last two valuesand returns in vector
+    vector<string> tokens;
+
+    istringstream ss(s);
+    string tmp;
+    while (ss >> tmp) {
+        tokens.push_back(tmp);
+        cout << tmp << endl;
+    }
+
+    cout << tokens[tokens.size() - 1] << endl;
+    cout << tokens[tokens.size() - 7] << endl;
+
+    vector<int> retvec{ 0, 0 };
+    return retvec;
+}
+
+void MainWindow::getDefaultPowercfg() {
+    string get_max_default = "powercfg Q SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX";
+    string get_min_default = "powercfg Q SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN";
+
+    QProcess process;
+    process.start("powercfg Q SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX");
+    process.waitForFinished(-1);
+
+    //string output = process.readAllStandardOutput().toStdString();
+    vector<int> maxs = parsePowercfgOutput(process.readAllStandardOutput().toStdString());
+
+    process.start("powercfg Q SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN");
+    process.waitForFinished(-1);
+
+    //output = process.readAllStandardOutput().toStdString();
+    vector<int> mins = parsePowercfgOutput(process.readAllStandardOutput().toStdString());
+
+
+    default_ACProcThrottleMin = mins[0];
+    default_DCProcThrottleMin = mins[1];
+    default_ACProcThrottleMax = maxs[0];
+    default_DCProcThrottleMax = maxs[1];
+
+}
+
+void MainWindow::setFreq(int p) {
+
+#ifdef _WIN32
+
+    std::string min_ac_str = "powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN " + std::to_string(p);
+    std::string min_dc_str = "powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMIN " + std::to_string(p);
+    std::string max_ac_str = "powercfg -setacvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX " + std::to_string(p);
+    std::string max_dc_str = "powercfg -setdcvalueindex SCHEME_CURRENT SUB_PROCESSOR PROCTHROTTLEMAX " + std::to_string(p);
+
+    WinExec(min_ac_str.c_str(), SW_HIDE);
+    WinExec(min_dc_str.c_str(), SW_HIDE);
+    WinExec(max_ac_str.c_str(), SW_HIDE);
+    WinExec(max_dc_str.c_str(), SW_HIDE);
+
+#endif
+
+    return;
 }
 
 void MainWindow::fillBar(QProgressBar* pb) {
@@ -38,9 +106,23 @@ void MainWindow::fillBar(QProgressBar* pb) {
 //#endif
 }
 
+void MainWindow::pickThrottledTask() {
+    srand((unsigned)time(NULL));
+    throttled_task = rand() % 2 + 2;
+    unthrottled_task = (throttled_task == 2) ? 3 : 2;
+}
+
 void MainWindow::showPatch1() {
     ui.stackedWidget->setCurrentWidget(ui.patch1);
+    pickThrottledTask();
     fillBar(ui.patch1->progress_bar);
+    pickThrottledTask();
+    if (throttled_task == 2) {
+        setFreq(100 - slowdown);
+    }
+    else {
+        setFreq(100);
+    }
     ui.patch1->done_label->setText("Done!");
     ui.patch1->continue_btn->setEnabled(true);
 }
--- Protocol1App/MainWindow.h
+++ Protocol1App/MainWindow.h
@@ -4,6 +4,11 @@
 #include <QProgressBar>
 #include "ui_MainWindow.h"
 
+#ifdef _WIN32
+#include <windows.h>
+#pragma comment(lib, "user32.lib")
+#endif
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -14,11 +19,25 @@ public:
 private:
     Ui::MainWindow ui;
 
+    int throttled_task = -1;
+    int unthrottled_task = -1;
+    int slowdown = 30;
+
+    int default_CsEnabled;
+    int default_ACProcThrottleMin;
+    int default_ACProcThrottleMax;
+    int default_DCProcThrottleMin;
+    int default_DCProcThrottleMax;
+
+    void getDefaultPowercfg();
+
     void showGoodbye();
     void showTask1();
     void showTask2();
     void showTask3();
+    void setFreq(int p);
     void fillBar(QProgressBar *qb);
+    void pickThrottledTask();
     void showPatch1();
     void showPatch2();
     void showPatch3();
