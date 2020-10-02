a30267395f7596cdf668d86fe8958efa24aa094f
adamahastings <hastings@cs.columbia.edu>
2020-10-02 08:54:56 -0400

    colons

--- Protocol2App/FormPage.cpp
+++ Protocol2App/FormPage.cpp
@@ -93,41 +93,41 @@ FormPage::FormPage(QWidget* parent)
 	name = new QLabel(this);
 	name->setGeometry(2 * M, 3 * M, 2 * M, BUTTON_HEIGHT);
 	name->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-	name->setText("Name  ");
+	name->setText("Name:  ");
 	line_name = new QLineEdit(this);
 	line_name->setGeometry(4 * M, 3 * M, 4 * M, BUTTON_HEIGHT);
 
 	uni = new QLabel(this);
 	uni->setGeometry(8 * M, 3 * M, 2 * M, BUTTON_HEIGHT);
 	uni->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
-	uni->setText("UNI  ");
+	uni->setText("UNI:  ");
 	line_uni = new QLineEdit(this);
 	line_uni->setGeometry(10 * M, 3 * M, 4 * M, BUTTON_HEIGHT);
 
 	address = new QLabel(this);
 	address->setGeometry(2 * M, 5 * M, 2 * M, BUTTON_HEIGHT);
-	address->setText("Address  ");
+	address->setText("Address:  ");
 	address->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
 	line_address = new QLineEdit(this);
 	line_address->setGeometry(4 * M, 5 * M, 10 * M, BUTTON_HEIGHT);
 
 	city = new QLabel(this);
 	city->setGeometry(2 * M, 6 * M, 2 * M, BUTTON_HEIGHT);
-	city->setText("City  ");
+	city->setText("City:  ");
 	city->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
 	line_city = new QLineEdit(this);
 	line_city->setGeometry(4 * M, 6 * M, 2 * M, BUTTON_HEIGHT);
 
 	state = new QLabel(this);
 	state->setGeometry(6 * M, 6 * M, 2 * M, BUTTON_HEIGHT);
-	state->setText("State  ");
+	state->setText("State:  ");
 	state->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
 	line_state = new QLineEdit(this);
 	line_state->setGeometry(8 * M, 6 * M, 2 * M, BUTTON_HEIGHT);
 
 	zip = new QLabel(this);
 	zip->setGeometry(10 * M, 6 * M, 2 * M, BUTTON_HEIGHT);
-	zip->setText("Zip  ");
+	zip->setText("Zip:  ");
 	zip->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
 	line_zip = new QLineEdit(this);
 	line_zip->setGeometry(12 * M, 6 * M, 2 * M, BUTTON_HEIGHT);
