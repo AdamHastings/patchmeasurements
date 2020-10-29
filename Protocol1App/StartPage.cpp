4f51774bc6af092c61c93cca25fa0c27f203ee68
adamahastings <hastings@cs.columbia.edu>
2020-10-29 18:42:31 -0400

    prototyping lie + debriefing on this lie

--- Protocol1App/StartPage.cpp
+++ Protocol1App/StartPage.cpp
@@ -17,7 +17,7 @@ StartPage::StartPage(QWidget *parent)
     not_consent_btn->setGeometry(QRect(W / 2 + M, M * 7, BUTTON_WIDTH, BUTTON_HEIGHT));
     not_consent_btn->setText("I do not consent");
 
-    label->setText("Thank you for participating in this experiment. This experiment will have you complete a few simple tasks on this computer. This experiment will take about 20 minutes to complete. After you complete the tasks, we will ask you a few questions about your experience. This experiment is intended to help the researchers better understand the economics of computer security.\n\nDo you consent to participate in this study? You may exit the experiment at any point.");
+    label->setText("Thank you for participating in this experiment. This experiment is designed to test how computer users respond to some computer system modifications we are prototyping. We will have you complete some simple tasks under a variety of different circumstances. After you complete the tasks, we will ask you a few questions about your experience. The experiment is self-guided and takes about 20 to 30 minutes to complete.\n\nDo you consent to participate in this study? You may exit the experiment at any point.");
 
     // TODO can we tell them they have to finish the experiment?
 }
