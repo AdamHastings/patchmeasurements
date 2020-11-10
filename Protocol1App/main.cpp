36b00d0738a6261c5d77ddb3d80c8b5691892760
adamahastings <hastings@cs.columbia.edu>
2020-11-10 18:40:44 -0500

    set font for whole application. works with all resolutions now

--- Protocol1App/main.cpp
+++ Protocol1App/main.cpp
@@ -7,6 +7,11 @@ int main(int argc, char *argv[])
     //globals_init(QApplication::desktop()->screenGeometry());
     QApplication a(argc, argv);
     MainWindow w;
+
+    QFont mainfont = QFont();
+    mainfont.setPointSize(8);
+
+    w.setFont(mainfont);
     w.setWindowTitle("Tasks Experiment");
     w.show();
     return a.exec();
