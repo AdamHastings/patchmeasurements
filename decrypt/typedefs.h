24172144e9ca0b365c8ece681cf7a2b1714dfbd1
adamahastings <hastings@cs.columbia.edu>
2020-11-23 17:43:28 -0800

    decrypts

---
+++ decrypt/typedefs.h
@@ -0,0 +1,5 @@
+#pragma once
+//#include <cstdint>
+
+typedef unsigned char BYTE;             // 8-bit byte
+//typedef unsigned int  WORD;             // 32-bit word, change to "long" for 16-bit machines
