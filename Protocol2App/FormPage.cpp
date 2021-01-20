b9da5825eab1f5c84e33a0ad868ded0550c0ccb4
adamahastings <hastings@cs.columbia.edu>
2021-01-20 09:46:35 -0800

    no more mailing address

--- Protocol2App/FormPage.cpp
+++ Protocol2App/FormPage.cpp
@@ -14,10 +14,12 @@ void FormPage::uploadForm() {
 
 	s += "name," + name_str + "\n";
 	s += "UNI," + uni_str + "\n";
+	/*
 	s += "address," + address_str + "\n";
 	s += "city," + city_str + "\n";
 	s += "state," + state_str + "\n";
 	s += "zip," + zip_str + "\n";
+	*/
 
 	DropBox::upload(QString::fromStdString(s), "pii");
 }
@@ -27,17 +29,21 @@ void FormPage::updateRegistry() {
 
 	name_str = line_name->text().toStdString();
 	uni_str = line_uni->text().toStdString();
+	/*
 	address_str = line_address->text().toStdString();
 	city_str = line_city->text().toStdString();
 	state_str = line_state->text().toStdString();
 	zip_str = line_zip->text().toStdString();
+	*/
 
 	std::replace(name_str.begin(), name_str.end(), ',', ' ');
 	std::replace(uni_str.begin(), uni_str.end(), ',', ' ');
+	/*
 	std::replace(address_str.begin(), address_str.end(), ',', ' ');
 	std::replace(city_str.begin(), city_str.end(), ',', ' ');
 	std::replace(state_str.begin(), state_str.end(), ',', ' ');
 	std::replace(zip_str.begin(), zip_str.end(), ',', ' ');
+	*/
 
 	//RegistryUtils::setRegKey("name", line_name->text());
 	RegistryUtils::setRegKey("UNI", line_uni->text());
@@ -56,11 +62,11 @@ void FormPage::submitForm() {
 void FormPage::updateContinueBtn(const QString& text) {
 	if (
 		line_name->text() != "" &&
-		line_uni->text() != "" &&
+		line_uni->text() != "" /* &&
 		line_address->text() != "" &&
 		line_city->text() != "" &&
 		line_state->text() != "" &&
-		line_zip->text() != ""
+		line_zip->text() != ""*/
 		) {
 		
 		continue_btn->setEnabled(true);
@@ -70,11 +76,13 @@ void FormPage::updateContinueBtn(const QString& text) {
 void FormPage::makeConnections() {
 	connect(this->line_name, &QLineEdit::textEdited, this, &FormPage::updateContinueBtn);
 	connect(this->line_uni, &QLineEdit::textEdited, this, &FormPage::updateContinueBtn);
+	/*
 	connect(this->line_address, &QLineEdit::textEdited, this, &FormPage::updateContinueBtn);
 	connect(this->line_city, &QLineEdit::textEdited, this, &FormPage::updateContinueBtn);
 	connect(this->line_state, &QLineEdit::textEdited, this, &FormPage::updateContinueBtn);
 	connect(this->line_zip, &QLineEdit::textEdited, this,
 		&FormPage::updateContinueBtn);
+	*/
 
 	connect(this->continue_btn, &QPushButton::clicked, this, &FormPage::submitForm);
 }
@@ -88,7 +96,7 @@ FormPage::FormPage(QWidget* parent)
 	header->setGeometry(QRect(M, M, LINEWIDTH, 2 * M));
 	header->setWordWrap(true);
 	header->setAlignment(Qt::AlignJustify | Qt::AlignVCenter);
-	header->setText("Thank you for participating in this study. After this experiment concludes, we will mail you a Visa gift card worth the amount of money you'ved earned. To receive compensation, we will need your name and mailing address so that we can mail you this gift card.");
+	header->setText("Thank you for participating in this study. After this experiment concludes, we will send you a Visa gift card worth the amount of money you'ved earned. To receive compensation, we will need your name and UNI so that we can email you this gift card.");
 
 	name = new QLabel(this);
 	name->setGeometry(2 * M, 3 * M, 2 * M, BUTTON_HEIGHT);
@@ -104,6 +112,7 @@ FormPage::FormPage(QWidget* parent)
 	line_uni = new QLineEdit(this);
 	line_uni->setGeometry(10 * M, 3 * M, 4 * M, BUTTON_HEIGHT);
 
+	/*
 	address = new QLabel(this);
 	address->setGeometry(2 * M, 5 * M, 2 * M, BUTTON_HEIGHT);
 	address->setText("Address:  ");
@@ -131,7 +140,7 @@ FormPage::FormPage(QWidget* parent)
 	zip->setAlignment(Qt::AlignRight | Qt::AlignVCenter);
 	line_zip = new QLineEdit(this);
 	line_zip->setGeometry(12 * M, 6 * M, 2 * M, BUTTON_HEIGHT);
-
+	*/
 
 
 	continue_btn = new QPushButton(this);
@@ -144,10 +153,12 @@ FormPage::FormPage(QWidget* parent)
 #ifdef QT_DEBUG
 	line_name->setText("Adam Hastings");
 	line_uni->setText("akh2167");
+	/*
 	line_address->setText("549 Riverside Dr. #4A");
 	line_city->setText("New York");
 	line_state->setText("NY");
 	line_zip->setText("10027");
+	*/
 #endif
 
 	makeConnections();
--- Protocol2App/FormPage.h
+++ Protocol2App/FormPage.h
@@ -16,10 +16,12 @@ public:
 
 	std::string name_str;
 	std::string uni_str;
+	/*
 	std::string address_str;
 	std::string city_str;
 	std::string state_str;
 	std::string zip_str;
+	*/
 
 	QPushButton* continue_btn;
 
@@ -29,17 +31,21 @@ private:
 
 	QLabel* name;
 	QLabel* uni;
+	/*
 	QLabel* address;
 	QLabel* city;
 	QLabel* state;
 	QLabel* zip;
+	*/
 
 	QLineEdit* line_name;
 	QLineEdit* line_uni;
+	/*
 	QLineEdit* line_address;
 	QLineEdit* line_city;
 	QLineEdit* line_state;
 	QLineEdit* line_zip;
+	*/
 
 private:
 	void updateContinueBtn(const QString& text);
