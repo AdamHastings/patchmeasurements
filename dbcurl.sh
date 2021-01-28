0ec3553f6b5510007253fea564a59cf0ec8738fe
adamhastings <itsadamhastings@gmail.com>
2021-01-28 12:03:02 -0500

    Cleaning house

--- dbcurl.sh
+++
@@ -1,2 +0,0 @@
-curl -X POST https://content.dropboxapi.com/2/fir "Authorization: Bearer xOqWEVy1vFwAAAAAAAAAAelKgR5QlY3_o8ysU5eR52MjYtke1KAFMfKvOAvgkjbR"     --header "Dropbox-API-Arg: {\"path\": \"/Homework/math/Matrices.txt\", \"autorename\": true}"     --header "Content-Type: application/octet-stream"     --data-binary @matrices.txt
-
--- mousetest/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [[ $PATH_SET == "" ]]; then
-    PATH_SET="set!"
-    PATH=$PATH:/home/adam/mxe/usr/bin
-    echo "PATH is $PATH"
-fi
-
