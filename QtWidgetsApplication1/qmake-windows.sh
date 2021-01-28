cc05dbacd583559159899b34903ef0de6144c5b6
adamhastings <itsadamhastings@gmail.com>
2021-01-28 12:07:20 -0500

    removing?

--- QtWidgetsApplication1/qmake-windows.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [[ $PATH_SET == "" ]]; then
-    PATH_SET="set!"
-    PATH=$PATH:/home/adam/mxe/usr/bin
-    echo "PATH is $PATH"
-fi
-
-~/mxe/usr/bin/i686-w64-mingw32.static-qmake-qt5
