854259f111d25c0736e0a359887dd616761fcc66
adamahastings <hastings@cs.columbia.edu>
2021-09-02 22:59:17 -0400

    make app run from location

--- Protocol2App/main.cpp
+++ Protocol2App/main.cpp
@@ -1,8 +1,13 @@
 #include "Protocol2App.h"
 #include <QtWidgets/QApplication>
+#include <filesystem>
+
 
 int main(int argc, char *argv[])
 {
+
+    std::filesystem::current_path("C:/Program Files/HastingsExperiment"); //setting path
+
     QApplication a(argc, argv);
     Protocol2App w;
     w.show();
